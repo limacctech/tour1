1028037e7d23db43ecb263bd29053596273d8229
limacctech <tech@limacc.com>
2025-11-24 21:19:30 -0500

    Initial

---
+++ data.js
@@ -0,0 +1,494 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance-from-diller-rd",
+      "name": "Entrance from Diller Rd",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.27297380529954474,
+          "pitch": 0.23646111966712802,
+          "rotation": 1.5707963267948966,
+          "target": "3-west-parking-lot"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5719326738402213,
+          "pitch": -0.5405433040450589,
+          "title": "Address",
+          "text": "2945 N Cole St"
+        }
+      ]
+    },
+    {
+      "id": "1-entrance-from-north-on-cole-st",
+      "name": "Entrance from North on Cole St",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08713965260282563,
+          "pitch": -0.026574186452783977,
+          "rotation": 0.7853981633974483,
+          "target": "4-east-parking-lot"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9754997342914251,
+          "pitch": 0.006489466718839765,
+          "title": "Address",
+          "text": "2945 N Cole St"
+        }
+      ]
+    },
+    {
+      "id": "2-entrance-from-lima-on-cole-st",
+      "name": "Entrance from Lima on Cole St",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05852166606584852,
+          "pitch": 0.14389448868031707,
+          "rotation": 4.71238898038469,
+          "target": "4-east-parking-lot"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.775091906742901,
+          "pitch": 0.08408075667484383,
+          "title": "Address",
+          "text": "2945 N Cole St"
+        }
+      ]
+    },
+    {
+      "id": "3-west-parking-lot",
+      "name": "West Parking Lot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18998242054181347,
+          "pitch": 0.0739708782698223,
+          "rotation": 0,
+          "target": "5-main-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-east-parking-lot",
+      "name": "East Parking Lot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15813644840029717,
+          "pitch": 0.13910886534866407,
+          "rotation": 0,
+          "target": "5-main-lobby"
+        },
+        {
+          "yaw": 0.6771157638272776,
+          "pitch": 0.09238641641402623,
+          "rotation": 5.497787143782138,
+          "target": "5-main-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-main-lobby",
+      "name": "Main Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.09414759222691949,
+        "pitch": -0.0737443432187117,
+        "fov": 1.5091423593016802
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.30539113803445517,
+          "pitch": 0.12275944926005522,
+          "rotation": 0,
+          "target": "7-sanctuary---center-aisle"
+        },
+        {
+          "yaw": 0.861768767300779,
+          "pitch": 0.2421099079949265,
+          "rotation": 0.7853981633974483,
+          "target": "6-cafe"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5717813757448553,
+          "pitch": 0.29048830383369406,
+          "title": "Website",
+          "text": "More info is available on our website - <a href=\"https://www.limacommunitychurch.com\" target=\"_blank\"><u>https://www.limacommunitychurch.com</u></a>"
+        }
+      ]
+    },
+    {
+      "id": "6-cafe",
+      "name": "Cafe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22779085838907953,
+          "pitch": 0.5680127060279432,
+          "rotation": 3.141592653589793,
+          "target": "5-main-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sanctuary---center-aisle",
+      "name": "Sanctuary - Center Aisle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08249118968120861,
+          "pitch": 0.6772660905352286,
+          "rotation": 3.141592653589793,
+          "target": "10-welcome-counter"
+        },
+        {
+          "yaw": 0.512226242354398,
+          "pitch": 0.5287358343768744,
+          "rotation": 1.5707963267948966,
+          "target": "8-sanctuary---east-aisle"
+        },
+        {
+          "yaw": -0.3195451928300166,
+          "pitch": 0.5293669719188792,
+          "rotation": 4.71238898038469,
+          "target": "9-sanctuary---west-aisle"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.09701227695301107,
+          "pitch": 0.40248553322259895,
+          "title": "Online Messages",
+          "text": "Link to our online messages - <a href=\"https://limacc.com/messages\" target=\"_blank\"><u>https://limacc.com/messages</u></a>"
+        }
+      ]
+    },
+    {
+      "id": "8-sanctuary---east-aisle",
+      "name": "Sanctuary - East Aisle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2605826260155979,
+          "pitch": 0.6208423507132323,
+          "rotation": 3.141592653589793,
+          "target": "10-welcome-counter"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sanctuary---west-aisle",
+      "name": "Sanctuary - West Aisle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15444106212788888,
+          "pitch": 0.6106700474952067,
+          "rotation": 3.141592653589793,
+          "target": "10-welcome-counter"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-welcome-counter",
+      "name": "Welcome Counter",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 1.2504502541394462,
+        "pitch": 0,
+        "fov": 1.5091423593016802
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.5682301623624753,
+          "pitch": 0.25951435119124966,
+          "title": "Worship Times",
+          "text": "Our Worship Services are on Sunday at 9:00 and 10:45AM."
+        },
+        {
+          "yaw": 1.5809779634066512,
+          "pitch": 0.5166657190365669,
+          "title": "More information",
+          "text": "More information is available and Pre-Registration for kiddos is available on our website - <a href=\"https://www.limacommunitychurch.com\" target=\"_blank\"><u>https://limacommunitychurch.com</u></a>"
+        }
+      ]
+    }
+  ],
+  "name": "LCCN Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
